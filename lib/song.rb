9287326760c33e6505c51807a7b9fa243030d59f
Hannah Angle <hannahcangle@gmail.com>
2020-08-08 02:40:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,4 +3,14 @@ attr_accessor :name, :artist, :genre
 @@count = 0 
 @@artists = []
 @@genres = []
+
+def initialize(song_name, artist, genre)
+  @name = song_name
+  @artist = artist
+  @genre = genre 
+  @@count += 1 
+  @@artists << artist 
+  @@genres << genre 
+end 
+
 end 
