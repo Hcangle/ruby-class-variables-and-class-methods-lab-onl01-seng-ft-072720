eca38a5f70c014646ff5f4ebdfdd8738b553ff9a
Hannah Angle <hannahcangle@gmail.com>
2020-08-08 02:42:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,5 +12,18 @@ def initialize(song_name, artist, genre)
   @@artists << artist 
   @@genres << genre 
 end 
-
+def self.count
+  @@count
+end
+def self.artists
+  @@artists.uniq 
+end
+def self. genres
+  @@genres.uniq 
+end
+def self.genre_count
+  genre_count = Hash.new(0)
+  @@genres.each {|genre| genre_count[genre] += 1}
+  genre_count
+end
 end 
